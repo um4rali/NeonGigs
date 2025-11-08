819cbbd5a1a40b7d4fe3e4c7fdd3ddb6b85c337c
Umar Ali <um4r4ali@gmail.com>
2025-11-08 18:00:29 +0530

    Update API URL to production endpoint

--- neongig-app/src/pages/GigDetails.js
+++ neongig-app/src/pages/GigDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://neongigs.onrender.com/api';
 
 export default function GigDetails() {
     const { id } = useParams();
