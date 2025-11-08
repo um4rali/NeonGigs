3e9ebaf7e00355ee4597f352631fc0e03de836d8
Umar Ali <um4r4ali@gmail.com>
2025-11-08 17:58:32 +0530

    Update AdminDashboard.js

--- neongig-app/src/pages/AdminDashboard.js
+++ neongig-app/src/pages/AdminDashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://neongigs.onrender.com/api';
 
 export default function AdminDashboard() {
     const [activeTab, setActiveTab] = useState('dashboard');
