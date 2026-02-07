5ae3d1580dd06d82590171b93876a80125d5dbf0
Umar Ali <um4r4ali@gmail.com>
2026-02-07 16:23:43 +0530

    Create OpenSSLLab component for RSA demonstration

---
+++ neongig-app/src/pages/OpenSSLLab.js
@@ -0,0 +1,8 @@
+export default function OpenSSLLab() {
+  return (
+    <div style={{ padding: "40px", color: "#eaf2ff", background: "#070a12", minHeight: "100vh" }}>
+      <h1>OpenSSL Security Lab</h1>
+      <p>This page will demonstrate RSA using OpenSSL.</p>
+    </div>
+  );
+}
